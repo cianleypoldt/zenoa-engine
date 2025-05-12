44a8a7342d4323631af1678ac288374e466a72cd
Cian Leypoldt <cianleypoldt@gmail.com>
2025-05-12 14:41:47 +0200

    initial commit

---
+++ build/CMakeFiles/Physics_engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Physics_engine.dir/link.d"
+  "CMakeFiles/Physics_engine.dir/src/main.cpp.o"
+  "CMakeFiles/Physics_engine.dir/src/main.cpp.o.d"
+  "Physics_engine"
+  "Physics_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Physics_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
